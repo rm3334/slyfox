f80703e560e62e156a7c8d6eb84f6eb1ed428b75
bbloom <bbloom@4455d1c2-61bb-8711-d336-a5ed47e11eef>
2011-04-02 00:11:05 +0000

    Still Adding Core Files

---
+++ trunk/SlyFox_AWG/+AnalogChannel/@AChanGroupFrontend/AChanGroupFrontend.m
@@ -0,0 +1,12 @@
+classdef AChanGroupFrontend
+    %ACHANGROUPFRONTEND Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
