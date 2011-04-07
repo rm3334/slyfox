f927aea4116ec57197fe110a0f45a66c8ba72038
bbloom <bbloom@4455d1c2-61bb-8711-d336-a5ed47e11eef>
2011-04-07 00:26:38 +0000

    Some Serious testing with AChan, but am now trying to test AChanGroup.m

--- trunk/SlyFox_AWG/+AnalogChannel/@AChanGroup/AChanGroup.m
+++ trunk/SlyFox_AWG/+AnalogChannel/@AChanGroup/AChanGroup.m
@@ -5,8 +5,9 @@ classdef AChanGroup < hgsetget
     %        numChannels (number)
     %        groupName (String)
     %        channelNames (Cell Array - List of Names)
+    %        adaptorName (String)
     %        HardwareProperties (Map)
-    %          Keys: DriverName, DevName
+    %          keys:Property Names, values: Property Values
     
     properties (SetAccess = private)
         myDevice = [];
@@ -18,22 +19,33 @@ classdef AChanGroup < hgsetget
     
     properties
         myNumChannels = 1;
-        myAChans = AChan('Ch0');
+        myAChans = AnalogChannel.AChan(0,'Ch0', 'nidaqmx');
         myName = 'Dev0'
         myDisplay = [];
         myChanNames = {'Ch0'};
+        myAdaptorName = 'nidaqmx'
+        myHWProperties = containers.Map(...
+            {'SampleRate',...
+            'TriggerType'},...
+            {50000,...
+            'HwDigital'});
     end
     
     methods
+%%%%%%%%%%%%%%%%%% CONSTRUCTOR %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         function obj = AChanGroup(varargin)
             if nargin >0
+                %%%%%%%%%%%%% Steps 1 - assign values to "myValues"
                 obj.myNumChannels = varargin{1};
                 obj.myName = varargin{2};
-                obj.myChanNames{:} = varargin{3}
-                %%%%%%%%%%%%% Steps 1 - assign values to "myValues"
-                %%%%%%%%%%%%% Steps 2 - safely create device
-                %%%%%%%%%%%%% Steps 3 - safely create channel objects
+                obj.myChanNames{:} = varargin{3};
+                obj.myHWProperties = varargin{4};
                 
+                %%%%%%%%%%%%% Steps 2 - safely create device
+                for k=1:obj.myNumChannels
+                    obj.addAChan(k,obj.myChanNames{k})
+                end
+                obj.buildNewDAQSession
             end
         end
 %%%%%%%%%%%%%%%%%% SET METHODS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -66,7 +78,15 @@ classdef AChanGroup < hgsetget
         function obj = set.myAChans(obj, value)
             obj.myAChans(:) = value;
         end
-        
+        function obj = set.myDevice(obj,value)
+            obj.myDevice = value
+        end
+        function obj = set.myHWProperties(obj, value)
+            obj.myHWProperties = value;
+        end
+        function obj = set.myAdaptorName(obj, value)
+            obj.myAdaptorName = value;
+        end
 %%%%%%%%%%%%%%%%%% ADD GET METHODS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         function value = get.myDevice(obj)
             value = obj.myDevice;
@@ -86,6 +106,13 @@ classdef AChanGroup < hgsetget
         function value = get.myChanNames(obj)
             value{:} = obj.myChanNames;
         end
+        function value = get.myHWProperties(obj)
+            value = obj.myHWProperties
+        end
+        function value = get.myAdaptorName(obj)
+            value = obj.myAdaptorName;
+        end
+       
 %%%%%%%%%%%%%%%%%%SMART DESTRUCTOR%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%% ACTIVE/BUILDER Functions%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         function bool = addAChan(obj, newChanID, newChanName)
@@ -96,8 +123,7 @@ classdef AChanGroup < hgsetget
             %Try to add to device
             try
                 addchannel(obj.myDevice, newChanID);
-                out = daqhwinfo(obj.myDevice);
-                newAChan = AChan(newChanID, newChanName, out.AdaptorName);
+                newAChan = AChan(newChanID, newChanName, obj.myAdaptorName);
                 set(obj, 'myAChans', [obj.myAChans; newAChan]);
                 bool = 1;
             catch exception
@@ -105,20 +131,97 @@ classdef AChanGroup < hgsetget
                 error('Channel could not be added to device object');
             end
         end
-% function enableAChan - should be public  Start/Stops output without
-%                       destroying waveform
-% function disableAChan - should be public Start/Stops output without
-%                       destroying waveform
-% function uploadData - should be public
+        function buildNewDAQSession(obj)
+            %BUILDNEWDAQSESSION Builds new DAQ and DAQ channels
+            %   This needs to be done whenever you want to change the the
+            %   output data as there is no "FLUSH DATA" function for the
+            %   DAQ. This is absolutely horrible and Mathworks should be
+            %   ashamed of themselves.....shuuuunnnn.
+            
+            %Check to see if a DAQ Device object is still around, if so
+            %delete and clear it.
+            if isvalid(obj.myDevice)
+                delete(obj.myDevice);
+                clear(obj.myDevice); %is this ok?
+            end
+            propNames = keys(obj.myHWProperties);
+            propValues = values(obj.myHWProperties, propNames);
+            obj.myDevice = analogoutput(obj.myAdaptor, obj.myName);
+            set(obj.myDevice, propNames, propValues); % Setting Global Properties
+            
+            
+            channelNames{:} = obj.myAChans{:}.myName; 
+            addchannels(obj.myDevice, 0:obj.myNumChannels, channelNames)% Adds correct number of channels
+            
+            ID{:} = obj.myAChans{:}.myIDnum;
+            
+            %Sets Channel DefaultVoltageValue
+            obj.myDevice.OutOfDataMode = 'DefaultValue';
+            defaultVval{:} = obj.myAChans{:}.myDefaultVoltageValue;
+            obj.myDevice.Channel(ID{:}).DefaultChannelValue = defaultVval{:};
+            
+            %Sets Hardware Trigger Channel
+            TrigChannels{:} = obj.myAChans{:}.myTriggerPort;
+            obj.myDevice.Channel(ID{:}).HwDigitalTriggerSource = TrigChannels{:};
+        end
+        function bool = uploadData(obj) %%%%% NOT DONE YET
+            try
+                if strcmp(get(obj.myDevice, 'Running'), 'On')
+                    stop(obj.myDevice);
+                end
+                %%%%NEED TO SET HW PROPERTIES AND CHANNEL TRIGGERS
+                data = obj.assembleOutputData();
+                putdata(obj.myDevice, data);
+                bool = 1;
+                start(obj.myDevice);
+            catch exception
+                bool = 0;
+            end
+        end
 % function loadAChanGroup - should be public WRONG? - MAYBE SHOULD BE AS
 % HIGH LEVEL AS POSSIBLE
 % function saveAChanGroup - should be public WRONG? - MAYBE SHOULD BE AS
 % HIGH LEVEL AS POSSIBLE
-%%%%%%%%%%%%%%%%%%HELPER FUNCTIONS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function assembleOutputData - should be private, needs to make everything
-% 
-% function findLongestWaveform - should be private
-% function possibleSampleRates - should be public
+% function enableAChan - should be public  Start/Stops output without
+%                       destroying waveform
+% function disableAChan - should be public Start/Stops output without
+%                       destroying waveform
+    end
+    %%%%%%%%%%%%%%%%%%HELPER FUNCTIONS%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    methods (Access = private)
+        
+        function tEnd = findLongestWaveform(obj)
+            times(:) = obj.myAChans(:).myWaveformTime %not ; for debugging purposes
+            tEnd = max(times);
+        end
+        
+        function maxSampleRate = findMaxSampleRate(obj)
+            %FINDMAXSAMPLERATE Finds maximum sample rate
+            %   This function finds the maximum sample rate assuming that
+            %   the only limiting constraints are 1) the length of the
+            %   longest waveform and 2) the maximum amount of samples that
+            %   can be stored by the Analog Out card given the number of
+            %   channels.
+            maxSamples = obj.myDevice.MaxSamplesQueued; %takes into account scaling with # of channels.
+            out = daqhwinfo(obj.myDevice);
+            maxCARDSampleRate = out.MaxSampleRate;
+            tend = obj.findLongestWaveform;
+            if maxCardSampleRate*tend > maxSamples
+                maxSampleRate = floor(maxSamples/tend);
+            else
+                maxSampleRate = maxCARDSampleRate;
+            end
+        end
+        function data = assembleOutputData(obj)
+            sampleRate = obj.myHWProperties('SampleRate');
+            tEnd = obj.findLongestWaveform;
+            samples = floor(tEnd*sampleRate)+1;
+            numChannels = obj.myNumChannels;
+            data = zeros([samples numChannels]); %Preallocate for speed
+            for k = 1:numChannels
+                data(:,k) = obj.myAChans(k).sampleWaveform(sampleRate, tEnd);
+            end %Populate each column of data
+        end
     end
     
 end
