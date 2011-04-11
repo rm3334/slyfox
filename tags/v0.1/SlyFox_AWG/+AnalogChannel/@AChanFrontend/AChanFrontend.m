4b42d3c2f0722b20ee65ae22d2fce84c4db00b0e
bbloom@gmail.com <bbloom@gmail.com@4455d1c2-61bb-8711-d336-a5ed47e11eef>
2011-04-11 22:00:19 +0000

    Can be used to control NI cards via Matlab Script

---
+++ tags/v0.1/SlyFox_AWG/+AnalogChannel/@AChanFrontend/AChanFrontend.m
@@ -0,0 +1,12 @@
+classdef AChanFrontend < uipanel
+    %ACHANFRONTEND Extends UIPANEL to create UI for ACHAN
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ tags/v0.1/SlyFox_AWG/SlyFox_AWG.m
@@ -0,0 +1,81 @@
+function varargout = SlyFox_AWG(varargin)
+% SLYFOX_AWG MATLAB code for SlyFox_AWG.fig
+%      SLYFOX_AWG, by itself, creates a new SLYFOX_AWG or raises the existing
+%      singleton*.
+%
+%      H = SLYFOX_AWG returns the handle to a new SLYFOX_AWG or the handle to
+%      the existing singleton*.
+%
+%      SLYFOX_AWG('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SLYFOX_AWG.M with the given input arguments.
+%
+%      SLYFOX_AWG('Property','Value',...) creates a new SLYFOX_AWG or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before SlyFox_AWG_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to SlyFox_AWG_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help SlyFox_AWG
+
+% Last Modified by GUIDE v2.5 01-Apr-2011 17:36:08
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @SlyFox_AWG_OpeningFcn, ...
+                   'gui_OutputFcn',  @SlyFox_AWG_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before SlyFox_AWG is made visible.
+function SlyFox_AWG_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to SlyFox_AWG (see VARARGIN)
+
+% Choose default command line output for SlyFox_AWG
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes SlyFox_AWG wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = SlyFox_AWG_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes when figure1 is resized.
+function figure1_ResizeFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.uipanel1, 'Position', [0,0,1,1]);
