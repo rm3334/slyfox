67ac4ae0df5732d6e9617aff97288b42882c9bb5
bbloom <bbloom@gmail.com>
2011-04-11 23:17:07 +0000

    testing for usb unit

---
+++ SlyFox_AWG/reloadData.m
@@ -0,0 +1,7 @@
+function reloadData( src, event, obj)
+%RELOADDATA Summary of this function goes here
+%   Detailed explanation goes here
+            putdata(src, obj.myTempData);
+            start(src);
+end
+
