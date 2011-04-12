bbf150f12c3e5d4f0d8221b7939d511dff8cadd9
bbloom <bbloom@4455d1c2-61bb-8711-d336-a5ed47e11eef>
2011-04-12 00:19:01 +0000

    works with usb-6259

--- trunk/SlyFox_AWG/testScript.m
+++ trunk/SlyFox_AWG/testScript.m
@@ -4,15 +4,15 @@ try
 catch exception
 end
 clear all
-a = AnalogChannel.AChanGroup(8,'Dev1', {'Ch0','Ch1','Ch2','Ch3','Ch4','Ch5','Ch6','Ch7'} ,'nidaq', containers.Map(...
+a = AnalogChannel.AChanGroup(4,'Dev1', {'Ch0','Ch1','Ch2','Ch3'} ,'nidaq', containers.Map(...
 {'SampleRate',...
 'TriggerType',...
 'HwDigitalTriggerSource', ...
 'TransferMode'},...
-{500000,...
+{100000,...
 'HwDigital',...
-'PFI0',...
-'SingleDMA'}))
+'PFI1',...
+'Interrupts'}))
 h1 = get(gcf);
 %%
 ch0 = a.myAChans{1};
@@ -76,66 +76,66 @@ subplot(2,4,4)
 plot(1000*temp3(:,1), temp3(:,2), 'LineWidth', 3)
 xlabel('ms')
 title(ch3.myName)
-%%
-ch4 = a.myAChans{5};
-ch4.myWaveformType = 'Linear';
-ch4.myDefaultVoltageValue = 0;
-tempWaveform4 = ...
-    [0.347, 80;...
-    0.347, 125.5;...
-    1.2, 55;...
-    1.2, 0.5];
-temp4 =[.001*[0; cumsum(tempWaveform4(:,2))], [tempWaveform1(:,1); ch4.myDefaultVoltageValue]];
-set(ch4, 'myWaveform',temp4)
-subplot(2,4,5)
-plot(1000*temp4(:,1), temp4(:,2), 'LineWidth', 3)
-xlabel('ms')
-title(ch4.myName)
-%%
-ch5 = a.myAChans{6};
-ch5.myWaveformType = 'Linear';
-ch5.myDefaultVoltageValue = 0;
-tempWaveform5 = ...
-    [0.347, 80;...
-    0.347, 125.5;...
-    1.2, 55;...
-    1.2, 0.5];
-temp5 =[.001*[0; cumsum(tempWaveform5(:,2))], [tempWaveform5(:,1); ch5.myDefaultVoltageValue]];
-set(ch5, 'myWaveform',temp5)
-subplot(2,4,6)
-plot(1000*temp5(:,1), temp5(:,2), 'LineWidth', 3)
-xlabel('ms')
-title(ch5.myName)
-%%
-ch6 = a.myAChans{7};
-ch6.myWaveformType = 'Linear';
-ch6.myDefaultVoltageValue = 0;
-tempWaveform6 = ...
-    [0.347, 80;...
-    0.347, 125.5;...
-    1.2, 55;...
-    1.2, 0.5];
-temp6 =[.001*[0; cumsum(tempWaveform6(:,2))], [tempWaveform6(:,1); ch6.myDefaultVoltageValue]];
-set(ch6, 'myWaveform',temp6)
-subplot(2,4,7)
-plot(1000*temp6(:,1), temp6(:,2), 'LineWidth', 3)
-xlabel('ms')
-title(ch6.myName)
-%%
-ch7 = a.myAChans{8};
-ch7.myWaveformType = 'Linear';
-ch7.myDefaultVoltageValue = 0;
-tempWaveform7 = ...
-    [0.347, 80;...
-    0.347, 125.5;...
-    1.2, 55;...
-    1.2, 0.5];
-temp7 =[.001*[0; cumsum(tempWaveform7(:,2))], [tempWaveform7(:,1); ch7.myDefaultVoltageValue]];
-set(ch7, 'myWaveform',temp7)
-subplot(2,4,8)
-plot(1000*temp7(:,1), temp7(:,2), 'LineWidth', 3)
-xlabel('ms')
-title(ch7.myName)
+% %%
+% ch4 = a.myAChans{5};
+% ch4.myWaveformType = 'Linear';
+% ch4.myDefaultVoltageValue = 0;
+% tempWaveform4 = ...
+%     [0.347, 80;...
+%     0.347, 125.5;...
+%     1.2, 55;...
+%     1.2, 0.5];
+% temp4 =[.001*[0; cumsum(tempWaveform4(:,2))], [tempWaveform1(:,1); ch4.myDefaultVoltageValue]];
+% set(ch4, 'myWaveform',temp4)
+% subplot(2,4,5)
+% plot(1000*temp4(:,1), temp4(:,2), 'LineWidth', 3)
+% xlabel('ms')
+% title(ch4.myName)
+% %%
+% ch5 = a.myAChans{6};
+% ch5.myWaveformType = 'Linear';
+% ch5.myDefaultVoltageValue = 0;
+% tempWaveform5 = ...
+%     [0.347, 80;...
+%     0.347, 125.5;...
+%     1.2, 55;...
+%     1.2, 0.5];
+% temp5 =[.001*[0; cumsum(tempWaveform5(:,2))], [tempWaveform5(:,1); ch5.myDefaultVoltageValue]];
+% set(ch5, 'myWaveform',temp5)
+% subplot(2,4,6)
+% plot(1000*temp5(:,1), temp5(:,2), 'LineWidth', 3)
+% xlabel('ms')
+% title(ch5.myName)
+% %%
+% ch6 = a.myAChans{7};
+% ch6.myWaveformType = 'Linear';
+% ch6.myDefaultVoltageValue = 0;
+% tempWaveform6 = ...
+%     [0.347, 80;...
+%     0.347, 125.5;...
+%     1.2, 55;...
+%     1.2, 0.5];
+% temp6 =[.001*[0; cumsum(tempWaveform6(:,2))], [tempWaveform6(:,1); ch6.myDefaultVoltageValue]];
+% set(ch6, 'myWaveform',temp6)
+% subplot(2,4,7)
+% plot(1000*temp6(:,1), temp6(:,2), 'LineWidth', 3)
+% xlabel('ms')
+% title(ch6.myName)
+% %%
+% ch7 = a.myAChans{8};
+% ch7.myWaveformType = 'Linear';
+% ch7.myDefaultVoltageValue = 0;
+% tempWaveform7 = ...
+%     [0.347, 80;...
+%     0.347, 125.5;...
+%     1.2, 55;...
+%     1.2, 0.5];
+% temp7 =[.001*[0; cumsum(tempWaveform7(:,2))], [tempWaveform7(:,1); ch7.myDefaultVoltageValue]];
+% set(ch7, 'myWaveform',temp7)
+% subplot(2,4,8)
+% plot(1000*temp7(:,1), temp7(:,2), 'LineWidth', 3)
+% xlabel('ms')
+% title(ch7.myName)
 %% run
 uploadData(a)
 % delete(a.myDevice) %for turning output off.
