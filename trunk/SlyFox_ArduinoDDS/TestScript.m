a19cfd469581f6a4d552b26af421260020109ded
bbloom@gmail.com <bbloom@gmail.com@4455d1c2-61bb-8711-d336-a5ed47e11eef>
2011-10-13 18:19:38 +0000

    half way through CHIRP mode

--- trunk/SlyFox_ArduinoDDS/TestScript.m
+++ trunk/SlyFox_ArduinoDDS/TestScript.m
@@ -1,11 +1,11 @@
 %% Testing Script for DDS stuff
 dds = DDS.DDS_Config(1)
 dds.myMode = 'Single Tone'
-[oFreq, ftw] = dds.calculateFTW(55.000000);
+[oFreq, ftw] = dds.calculateFTW(50.000000)
 params = struct('FTW1', ftw);
 iSet = dds.createInstructionSet('Single Tone', params)
-fwrite(s, iSet)
-fscanf(s)
+% fwrite(s, iSet)
+% fscanf(s)
 
 
 %% Testing Frontend
