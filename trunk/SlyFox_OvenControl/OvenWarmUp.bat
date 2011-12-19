aaf376b7cb84b8ea1a4f71b6f06b9886d749d8dc
bbloom@gmail.com <bbloom@gmail.com@4455d1c2-61bb-8711-d336-a5ed47e11eef>
2011-12-19 23:51:57 +0000

    Fixed pointing

--- trunk/SlyFox_OvenControl/OvenWarmUp.bat
+++ trunk/SlyFox_OvenControl/OvenWarmUp.bat
@@ -1,3 +1,3 @@
 @echo off
-matlab -automation -r "cd('C:\Users\Ben\Documents\MATLAB\SlyFox\SlyFox_OvenControl'); OvenWarmUp; quit"
+matlab -automation -r "cd('Z:\Sr3\slyfox\Slyfox_OvenControl'); OvenWarmUp; quit"
 exit
