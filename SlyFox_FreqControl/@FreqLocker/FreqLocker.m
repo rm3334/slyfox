3f8181ff84744fe00c2d29fd17b5942943f719c2
bbloom <bbloom@gmail.com>
2012-01-21 01:17:57 +0000

    Adding PID stuff

---
+++ SlyFox_FreqControl/@FreqLocker/FreqLocker.m
@@ -0,0 +1,12 @@
+classdef FreqLocker
+    %FREQLOCKER Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
---
+++ SlyFox_FreqControl/runFControl.m
@@ -0,0 +1,3 @@
+run './Main/AddPath.m'
+FrequencyControlMain(1);
+%Testing commit
