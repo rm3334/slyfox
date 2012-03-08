894f4e190b0f454a6f85138cc57eeaa864942d07
bbloom@gmail.com <bbloom@gmail.com@4455d1c2-61bb-8711-d336-a5ed47e11eef>
2012-03-08 17:25:33 +0000

    Changes for Streaming

--- trunk/SlyFox_FreqControl/FrequencyControlMain.m
+++ trunk/SlyFox_FreqControl/FrequencyControlMain.m
@@ -14,22 +14,39 @@ function FrequencyControlMain(DEBUGMODE )
     setappdata(gcf, 'DEBUGMODE', DEBUGMODE);
     tp = uiextras.TabPanel('Parent', pan);
     %Build Objects
-    g1 = GageCard.GageConfigFrontend(f,tp);
-    f1 = FreqSynth(f,tp);
-    ard = uControlFrontend(f, tp);
-    fs1 = FreqSweeper(f,tp);
-        fs1.setFreqSynth(f1);
-        fs1.setGageConfigFrontend(g1);
-        fs1.setuControl(ard);
-    fL = FreqLocker(f, tp);
-        fL.setFreqSynth(f1);
-        fL.setGageConfigFrontend(g1);
-        fL.setFreqSweeper(fs1);
-        fL.setuControl(ard);
-    gs = GageStreamerClientFrontend(f, tp);
-        gs.setFreqSweeper(fs1);
-        gs.setFreqLocker(fL);
-    tp.TabNames = {'Gage', 'FreqSynth',  'Arduino', 'FreqSweeper', 'FreqLocker', 'GageStreamer'};
+    switch DEBUGMODE
+        case {0,1}
+            g1 = GageCard.GageConfigFrontend(f,tp);
+            f1 = FreqSynth(f,tp);
+            ard = uControlFrontend(f, tp);
+            fs1 = FreqSweeper(f,tp);
+                fs1.setFreqSynth(f1);
+                fs1.setGageConfigFrontend(g1);
+                fs1.setuControl(ard);
+            fL = FreqLocker(f, tp);
+                fL.setFreqSynth(f1);
+                fL.setGageConfigFrontend(g1);
+                fL.setFreqSweeper(fs1);
+                fL.setuControl(ard);
+            gs = GageStreamerClientFrontend(f, tp);
+                gs.setFreqSweeper(fs1);
+                gs.setFreqLocker(fL);
+            tp.TabNames = {'Gage', 'FreqSynth',  'Arduino', 'FreqSweeper', 'FreqLocker', 'GageStreamer'};
+        case 2
+            f1 = FreqSynth(f,tp);
+            ard = uControlFrontend(f, tp);
+            fs1 = FreqSweeper(f,tp);
+                fs1.setFreqSynth(f1);
+                fs1.setuControl(ard);
+            fL = FreqLocker(f, tp);
+                fL.setFreqSynth(f1);
+                fL.setFreqSweeper(fs1);
+                fL.setuControl(ard);
+            gs = GageStreamerClientFrontend(f, tp);
+                gs.setFreqSweeper(fs1);
+                gs.setFreqLocker(fL);
+            tp.TabNames = {'FreqSynth',  'Arduino', 'FreqSweeper', 'FreqLocker', 'GageStreamer'};
+    end
     tp.TabSize = 100;
 
     mm = uimenu('Label', 'File');
@@ -38,21 +55,27 @@ function FrequencyControlMain(DEBUGMODE )
     set(f, 'ResizeFcn', @resizeTopPanel);
 
     function menuSaveFcn(src, event)
-        g1.saveState();
+        if DEBUGMODE ~= 2
+            g1.saveState();
+        end
         f1.saveState();
         fs1.saveState();
         fL.saveState();
     end
 
     function menuLoadFcn(src, event)
-        g1.loadState();
+        if DEBUGMODE ~=2
+            g1.loadState();
+        end
         f1.loadState();
         fs1.loadState();
         fL.loadState();
     end
 
     function windowClose(src,event)
-        g1.quit();
+        if DEBUGMODE ~= 2
+            g1.quit();
+        end
         f1.quit();
         fs1.quit();
         fL.quit();
